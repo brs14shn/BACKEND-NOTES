6b1575b67891b24bf056d02ff577e3be3de2cd9b
brs14shn <brsshn292929@gmail.com>
2022-08-24 23:06:57 +0300

    updated

--- django/008_Forms_Media_Bootstrap/student/views.py
+++ django/008_Forms_Media_Bootstrap/student/views.py
@@ -23,21 +23,34 @@ def index(request):
 #     }
 #     return render(request, 'student/student.html', context)
 
+# def student_page(request):
+#     form = StudentForm()
+#     if request.method == "POST":
+#         form = StudentForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             student_data = {
+#                 "first_name": form.cleaned_data.get('first_name'),
+#                 "last_name": form.cleaned_data.get("last_name"),
+#                 "number": form.cleaned_data.get("number"),
+#                 "profile_pic": form.cleaned_data.get("profile_image")
+#             }
+#             # Student.objects.create(**student_data)
+#             student = Student(**student_data)
+#             student.save()
+#             return redirect("index")
+#     context = {
+#         'form': form
+#     }
+
+#     return render(request, 'student/student.html', context)
+
 def student_page(request):
     form = StudentForm()
     if request.method == "POST":
         form = StudentForm(request.POST, request.FILES)
         if form.is_valid():
-            student_data = {
-                "first_name": form.cleaned_data.get('first_name'),
-                "last_name": form.cleaned_data.get("last_name"),
-                "number": form.cleaned_data.get("number"),
-                "profile_pic": form.cleaned_data.get("profile_image")
-            }
-            # Student.objects.create(**student_data)
-            student = Student(**student_data)
-            student.save()
-            return redirect("index")
+            form.save()
+            return redirect("student")
     context = {
         'form': form
     }
