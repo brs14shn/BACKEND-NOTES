0f7ed9f66e143b1b9ad7f48db2a6fd3dbfa39470
brs14shn <brsshn292929@gmail.com>
2022-08-31 21:22:27 +0300

    register

--- django/012_DJ_Auth_1/user_example/urls.py
+++ django/012_DJ_Auth_1/user_example/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from .views import (
     home,
-    special
+    special,
+    register
 )
 
 urlpatterns = [
     path('', home, name="home"),
     path('special/', special, name="special"),
+    path("register/",register,name="register")
 ]
--- django/012_DJ_Auth_1/user_example/views.py
+++ django/012_DJ_Auth_1/user_example/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 
 def home(request):
@@ -6,3 +7,11 @@ def home(request):
 
 def special(request):
     return render(request, "user_example/special.html")
+
+def register(request):
+    form=UserCreationForm()
+    context={
+        "form":form
+    }
+
+    return render(request,"registration/register.html",context)
