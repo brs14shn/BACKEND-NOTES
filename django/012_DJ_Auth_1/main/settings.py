06922f79c7300c3e83584068fce41d9b94411622
brs14shn <brsshn292929@gmail.com>
2022-08-31 22:25:08 +0300

    updated

--- django/012_DJ_Auth_1/main/settings.py
+++ django/012_DJ_Auth_1/main/settings.py
@@ -32,13 +32,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'user_example',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'user_example',
+    
 ]
 
 MIDDLEWARE = [
--- django/012_DJ_Auth_1/user_example/urls.py
+++ django/012_DJ_Auth_1/user_example/urls.py
@@ -4,9 +4,11 @@ from .views import (
     special,
     register
 )
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', home, name="home"),
     path('special/', special, name="special"),
-    path("register/",register,name="register")
+    path("register/",register,name="register"),
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name="registration/password_change.html"), name="password_change")
 ]
