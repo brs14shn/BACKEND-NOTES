bf09f13ab5d436e95c64b8afa76ec767d3d5fd48
brs14shn <brsshn292929@gmail.com>
2022-08-31 21:57:14 +0300

    updated

--- django/012_DJ_Auth_1/user_example/views.py
+++ django/012_DJ_Auth_1/user_example/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate,login
+
 
 
 def home(request):
@@ -9,7 +11,24 @@ def special(request):
     return render(request, "user_example/special.html")
 
 def register(request):
-    form=UserCreationForm()
+    form=UserCreationForm(request.POST or None) #? 👇 UZUN HALİ
+    
+    #! if request.method=="POST": #===========👆=======KISA HALİ
+    #! form=UseUserCreationForm(request.POST)
+    
+    if form.is_valid():
+        form.save()
+        username=form.cleaned_data["username"]
+        # username=form.cleaned_data.get("username")
+        password=form.cleaned_data["password1"]
+        # password=form.cleaned_data.get("password1")
+        user=authenticate(username=username,password=password)
+
+        login(request,user)
+        return redirect("home")
+        # return redirect("login")
+
+
     context={
         "form":form
     }
