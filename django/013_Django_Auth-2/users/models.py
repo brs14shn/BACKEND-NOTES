7ba2706f21234d2fb2d4abd6e0a33d289477b8f5
brs14shn <brsshn292929@gmail.com>
2022-09-01 21:27:34 +0300

    updated

--- django/013_Django_Auth-2/users/models.py
+++ django/013_Django_Auth-2/users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class  UsersProfile(models.Model):
+class  UserProfile(models.Model):
     portfolio = models.URLField(blank=True)
     profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
