9e020315a2a72e99ba3e042cf5c0634c9f4652a4
brs14shn <brsshn292929@gmail.com>
2022-09-01 21:08:40 +0300

    updated

--- django/013_Django_Auth-2/users/models.py
+++ django/013_Django_Auth-2/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class  UsersProfile(models.Model):
+    portfolio = models.URLField(blank=True)
+    profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+     
+     #burada auth içinden gelen user modelini kullanıp eşitledik
+    def __str__(self):
+        return self.user.username
