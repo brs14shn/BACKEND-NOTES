5ea476a9d9b644070aa727bde656137d892aa1d7
brs14shn <brsshn292929@gmail.com>
2022-09-17 11:52:06 +0300

    models and admin add

---
+++ django/classnotes/016_Serializers/student_api/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    number = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} - {self.last_name} "
