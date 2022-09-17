335b817e0f156f3e18cef799122e14affc3d03d8
brs14shn <brsshn292929@gmail.com>
2022-09-17 13:26:08 +0300

    path model created

--- django/classnotes/016_Serializers/student_api/serializers.py
+++ django/classnotes/016_Serializers/student_api/serializers.py
@@ -15,3 +15,16 @@ class StudentSerializer(serializers.Serializer):
         instance.number = validated_data.get('number', instance.number)
         instance.save()
         return instance
+
+#* =====================MODEL SERİALİZER========================
+class StudentSerializer(serializers.ModelSerializer):
+    full_name=serializers.SerializerMethodField()
+
+    def get_full_name(self,obj):
+        return f"{obj.first_name} {obj.last_name}"
+    class Meta:
+        model= Student
+        fields = ['id',"full_name" ,'first_name', 'last_name', 'number']
+        #fields = '__all__'
+        # exclude = ['id'] / ('id',) list veya tuple olarak yazılabilir.
+        # 
