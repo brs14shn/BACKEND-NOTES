307dbed2496a45382fe9b907a0839b724f129832
brs14shn <brsshn292929@gmail.com>
2022-09-27 16:41:20 +0300

    updated

--- django/projects/002_Todo_App/todo/static/todo/js/timeout.js
+++
@@ -1,5 +0,0 @@
-let element = document.querySelector('.message');
-
-setTimeout(function () {
-  element.style.display = 'none';
-}, 3000);
--- django/projects/002_Todo_App/todo/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import home, todo_create, todo_update, todo_delete
-
-urlpatterns = [
-    path("", home, name="home"),
-    path("add/", todo_create, name="add"),
-    path("update/<int:id>", todo_update, name="update"),
-    path("delete/<int:id>", todo_delete, name="delete"),
-]
