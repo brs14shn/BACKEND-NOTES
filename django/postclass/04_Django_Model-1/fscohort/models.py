02af385893c3b6bb0fa759ee2e3c4b0c26325ba9
brs14shn <brsshn292929@gmail.com>
2022-12-20 23:04:24 +0300

    model created

--- django/postclass/04_Django_Model-1/fscohort/models.py
+++ django/postclass/04_Django_Model-1/fscohort/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    number = models.IntegerField(default=1)
+    about = models.TextField(blank=True, null=True)
+    register_date=models.DateTimeField(auto_now_add=True)  #auto_now_add=True: it will be added automatically when the object is created
+    last_update_date=models.DateTimeField(auto_now=True) #auto_now=True: it will be added automatically when the object is updated
+    is_active = models.BooleanField(default=True)
+    
+    
+    def get_register_date(self):
+        return self.register_date.strftime("%d/%m/%Y")
+    
+    def get_last_update_date(self):
+        return self.last_update_date.strftime("%d/%m/%Y")
+    
+    def student_year_status(self):
+        "Returns the student's year status."
+        import datetime
+        if self.register_date < datetime.date(2019, 1, 1):
+            return "Senior"
+        elif self.register_date < datetime.date(2020, 1, 1):
+            return "Junior"
+        else:
+            return "Freshman"
+
+    def __str__(self):
+        return f"{self.number} - {self.first_name}"
+    class Meta:
+        ordering = ["number"]
+        verbose_name_plural = "student_list"
+    
--- django/postclass/04_Django_Model-1/fscohort/views.py
+++ django/postclass/04_Django_Model-1/fscohort/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    context = {
+        'caption': 'clarusway',
+        'dict1': {'django': 'best framework'},
+        'my_list': [2, 3, 4]
+    }
+    return render(request, 'fscohort/index.html', context)
